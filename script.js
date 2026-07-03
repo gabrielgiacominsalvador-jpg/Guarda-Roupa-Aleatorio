6a72d4382bc34857e00afefa09748e1deefe3ee7
GabiOfLoop <gabriel.giacomin.salvador@escola.pr.gov.br>
2026-07-03 12:27:17 -0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function revelarMensagem() {
-    document.getElementById("mensagem").innerText = "Olá, você alterou a mensagem chamando uma função JS"
-}
-
-function mostrarNome() {
-    let nome = document.getElementById("nomeUsuario").value;
-
-    document.getElementById("resultadoNome").innerText = "Bem-vindo, " + nome + "!";
-}
