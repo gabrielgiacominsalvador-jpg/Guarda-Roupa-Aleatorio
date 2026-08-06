39cbac30ea854f309a04bc01f68a0482a54f62c0
GabiOfLoop <gabriel.giacomin.salvador@escola.pr.gov.br>
2026-08-06 09:49:20 -0300

    Update script.js

--- ex11/script.js
+++ ex11/script.js
@@ -18,3 +18,27 @@ if (limite < 1 || limite > 100) {
     mensagem.style.color = "#b3261e";
     return;
   }
+
+let contagem = "";
+
+for (let numero = 1; numero <= limite; numero++) {
+        contagem += numero;
+
+        if (numero < limite) {
+            contagem += " - ";
+        }
+    }
+
+    mensagem.innerText = "Contagem gerada com sucesso!";
+    mensagem.style.color = "green";
+    resultado.innerText = contagem;
+}
+
+function limparCampos() {
+    document.getElementById("limite").value = "";
+    document.getElementById("mensagem").innerText =
+        "Informe um número para começar.";
+    document.getElementById("mensagem").style.color = "#52606d";
+    document.getElementById("resultado").innerText = "";
+    document.getElementById("limite").focus();
+}
