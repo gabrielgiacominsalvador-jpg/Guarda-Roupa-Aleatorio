81a393f7c6ce3a2ef2e651615a5ce937ff63c091
GabiOfLoop <gabriel.giacomin.salvador@escola.pr.gov.br>
2026-08-06 09:46:40 -0300

    Update script.js

--- ex11/script.js
+++ ex11/script.js
@@ -1,1 +1,20 @@
+function gerarContagem() {
+  let limiteDigitado = document.getElementById("limite").value;
+  let mensagem = document.getElementById("mensagem");
+  let resultado = document.getElementById("resultado");
 
+  resultado.innerText = "";
+
+  if (limiteDigitado === "") {
+    mensagem.innerText = "Digite o número final da contagem.";
+    mensagem.style.color = "#b3261e";
+    return;
+  }
+
+let limite = Number (limiteDigitado);
+
+if (limite < 1 || limite > 100) {
+    mensagem.innerText = "Digite um número entre 1 e 100.";
+    mensagem.style.color = "#b3261e";
+    return;
+  }
